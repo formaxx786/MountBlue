5fe56291d54497a80e53213c70dd19096545aeb2
Suneel <suneelmg7@gmail.com>
2026-08-25 15:42:57 +0530

    Add Problem-31: Electronics Shop with initial implementation and update ProblemLinks.md

--- Day8/ProblemLinks.md
+++ Day8/ProblemLinks.md
@@ -3,3 +3,8 @@ https://www.hackerrank.com/contests/mountblue-technologies/challenges/minimum-ab
 
 Problem-30: Mars Exploration
 https://www.hackerrank.com/contests/mountblue-technologies/challenges/mars-exploration/
+
+Problem-31: Electronics Shop
+https://www.hackerrank.com/contests/mountblue-technologies/challenges/electronics-shop/
+
+Problem-32: 
---
+++ Day8/problem31.cpp
@@ -0,0 +1,24 @@
+// Electronics Shop
+/*
+int getMoneySpent(vector<int> keyboards, vector<int> drives, int b) {
+  vector<int>x;
+  for(int i:keyboards){
+    for(int j:drives){
+        int p=i+j;
+        x.push_back(p);
+    }
+  }
+  int t=-1;
+  for(int i:x){
+    if(i<=b)
+    {
+        
+       if(i>=t)
+       {
+        t=i;
+       }
+    }
+  }
+return t;
+}
+*/
