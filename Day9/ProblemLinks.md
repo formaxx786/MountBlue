06aa88a189db69f841b25c81ab0e6120d1e7b32b
Suneel <suneelmg7@gmail.com>
2026-09-15 23:58:31 +0530

    Add Problem-34: Game of Stones with initial implementation and update ProblemLinks.md

--- Day9/ProblemLinks.md
+++ Day9/ProblemLinks.md
@@ -1,2 +1,5 @@
 Problem-33: Jumping on the Clouds
 https://www.hackerrank.com/contests/mountblue-technologies/challenges/jumping-on-the-clouds-revisited/
+
+Problem-34: Game of Stones
+https://www.hackerrank.com/contests/mountblue-technologies/challenges/game-of-stones-1/
---
+++ Day9/problem34.cpp
@@ -0,0 +1,27 @@
+// Game of Stones
+/*
+bool primee(int n){
+     if (n <= 1) {
+        return false;
+    }
+    if (n == 2) {
+        return true;
+    }
+    if (n % 2 == 0) {
+        return false;
+    }
+    for (int i = 3; i <= sqrt(n); i += 2) {
+        if (n % i == 0) {
+            return false; 
+        }
+    }
+    
+    return true;
+}
+string gameOfStones(int n) {
+   
+if(n%7==0||n%7==1)
+return "Second";
+return "First";
+}
+*/
