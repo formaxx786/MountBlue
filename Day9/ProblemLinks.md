8c253decfcb37ecaabb8bc6e8ce6f49fa77eccf8
Suneel <suneelmg7@gmail.com>
2026-09-15 23:55:41 +0530

    Add Problem-33: Jumping on the Clouds with initial implementation and update ProblemLinks.md; create tasks.json for C/C++ build configuration

---
+++ Day9/ProblemLinks.md
@@ -0,0 +1,2 @@
+Problem-33: Jumping on the Clouds
+https://www.hackerrank.com/contests/mountblue-technologies/challenges/jumping-on-the-clouds-revisited/
---
+++ Day9/problem33.cpp
@@ -0,0 +1,18 @@
+// jumping On Clouds
+
+/*
+int jumpingOnClouds(vector<int> c, int k){
+    int n=c.size();
+    int ans=100;
+    int i=0;
+    do {
+          i=(i+k)%n;
+        if(c[i]==1) 
+        ans-=3;
+        else 
+        ans-=1;
+      
+    } while(i!=0);
+    return ans;
+}
+*/
